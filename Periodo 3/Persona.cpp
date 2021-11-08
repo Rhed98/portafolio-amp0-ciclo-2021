ad1e41222c1e30b47a9fa6e94bb133d3731fb2ee
Rhene Galan <redgalan45@gmail.com>
2021-11-08 07:26:58 -0600

    Mi tercer commit

---
+++ Periodo 3/Persona.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+class Persona{
+	
+private:
+	string nombres;
+	string apellidos;
+	int anioN;
+			
+public:
+	Persona(string nombres, string apellidos, int anioNacimiento);
+	string nombreCompleto();
+	int edad();
+};
+	
+Persona::Persona(string nombres, string apellidos, int anioNacimiento){
+	this-> nombres = nombres;
+	this-> apellidos = apellidos;
+	this-> anioN = anioN;
+}
+
+string Persona::nombreCompleto(){
+	string nombreCompleto = this-> nombres + " " + this-> apellidos;
+	return nombreCompleto;
+}
+
+Persona::edad(){
+	
+	time_t now = time(0);
+	tm* ltm = localtime(&now);
+	
+	int anioActual = 1900 + ltm->tm_year;
+	
+	int edad = (anioActual - this-> anioN);
+	return edad;
+}
+
+int main(){
+	string nombres;
+	string apellidos;
+	int anioN;
+	
+	cout << "Ingrese sus Nombre: ";
+	getline(cin, nombres);
+	
+	cout << "Ingrese sus Apellidos: ";
+	getline(cin, apellidos);
+	
+	cout << "Ingrese su Anio de Nacimeinto: ";
+	cin >> anioN;
+	
+	Persona persona1(nombres, apellidos, anioN);
+	cout << endl << "-----DATOS DE LA PERSONA-----" <<endl;
+	cout << "Nombre: " << persona1.nombreCompleto() <<endl;
+	cout << "Edad: " << persona1.edad() << endl;
+	
+	return 0;
+}
